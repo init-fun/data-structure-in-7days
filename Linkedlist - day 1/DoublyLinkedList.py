f69c9f3c2822782dfe310d4fa3fc7a80629e8fca
inti-fun <sde.kapilraj@gmail.com>
2021-04-06 10:28:03 +0530

    linkedlist-day1
    Singly, doubly, cyclic

---
+++ Linkedlist - day 1/DoublyLinkedList.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, data, next=None, prev=None):
+        self.data = data
+        self.next = next
+        self.prev = prev
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
---
+++ Linkedlist - day 1/SinglyLinkedList.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+    
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+    
+    
