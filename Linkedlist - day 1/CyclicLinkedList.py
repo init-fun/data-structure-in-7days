07b38c57c7af084a70e75806f355323daa02b1a6
inti-fun <sde.kapilraj@gmail.com>
2021-04-06 22:47:24 +0530

    Update CyclicLinkedList.py

--- Linkedlist - day 1/CyclicLinkedList.py
+++ Linkedlist - day 1/CyclicLinkedList.py
@@ -9,7 +9,7 @@ class CyclicLinkedList:
         self.last = None
 
     def traverse(self):
-        if self.last == None:
+        if self.last is None:
             print("List is empty")
             return
 
@@ -45,9 +45,93 @@ class CyclicLinkedList:
         if self.last is None:
             print("List is empty")
             return
-        if self.last.data == data and self.last.next is self.last:
-            # single node
-            new_node = Node(new_data)
-            new_node.next = self.last.next
-            self.last = new_node
+
+        cnode = self.last.next
+        while True:
+            if cnode.data == data:
+                break
+            cnode = cnode.next
+            if cnode is self.last.next:
+                break
+
+        if cnode is self.last.next and cnode.data != data:
+            print(f"{data} not found in the list")
             return
+        else:
+            new_node = Node(new_data)
+            new_node.next = cnode.next
+            cnode.next = new_node
+            if cnode is self.last:
+                self.last = new_node
+
+    def insert_before(self, data, new_data):
+        if self.last is None:
+            print("List is empty")
+            return
+        cnode = self.last.next
+        prev_node = self.last
+        while True:
+            if cnode.data == data:
+                break
+            prev_node = cnode
+            cnode = cnode.next
+            if cnode is self.last.next:
+                break
+        if cnode is self.last.next and cnode.data != data:
+            print(f"{data} is not present in the lsit")
+        else:
+            # the ref is on the cnode
+            new_node = Node(new_data)
+            new_node.next = cnode
+            prev_node.next = new_node
+            prev_node = new_node
+        return
+
+    def delete_begining_node(self):
+        if self.last is None:
+            print("List is empty")
+            return
+        if self.last.next is self.last:
+            # in case of single node
+            self.last = None
+            return
+        self.last.next = self.last.next.next
+        return
+
+    def delete_last_node(self):
+        if self.last is None:
+            print("List is empty")
+            return
+
+        if self.last.next == self.last:
+            # single node
+            self.last = None
+            return
+
+        cnode = self.last.next
+        while cnode.next is not self.last:
+            cnode = cnode.next
+        cnode.next = self.last.next
+        self.last = cnode
+        return
+
+    def delete_this_node(self, data):
+        if self.last is None:
+            print("List is empty")
+            return
+        if self.last.next is self.last:
+            self.last = None
+            return
+
+        cnode = self.last.next
+        while cnode.next is not self.last.next:
+            if cnode.next.data == data:
+                break
+            cnode = cnode.next
+        if cnode.next == self.last.next:
+            print(f"{data} is not present in the list")
+        else:
+            cnode.next = cnode.next.next
+            if self.last.data == data:
+                self.last = cnode
+        return
